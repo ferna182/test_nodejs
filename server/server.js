58c8a30c21e4aa45e7e3906e10243b0b09d6357f
Fernandod <ferna182@gmail.com>
2018-11-05 00:04:33 -0300

    added mongodb location for heroku

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ app.use(bodyParser.json())
 // DB
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/book_db')
+const mongoLocation = process.env.MONGODB_URI || 'mongodb://localhost:27017/book_db'
+mongoose.connect(mongoLocation)
 
 const { Book } = require('./models/books')
 const { Store } = require('./models/stores')
